7371c4766484552723cd2e29bb72e6b978558d90
Shubh Raj <shubhraj625@gmail.com>
2025-12-13 23:47:55 +0530

    Observable

--- notificationSystem.cpp
+++ notificationSystem.cpp
@@ -67,6 +67,81 @@ public:
     }
 };
 
+//Observer Pattern
+
+class IObserver{
+    public:
+    virtual void update() = 0;
+    virtual ~IObserver() {};
+};
+
+class IObservable {
+public:
+    virtual void addObserver(IObserver* observer) = 0;
+    virtual void removeObserver(IObserver* observer) = 0;
+    virtual void notifyObservers() = 0;
+};
+
+class NotificationObservable :  public IObservable {
+private:
+    vector<IObserver*> observers;
+    INotification* currentNotification;
+public:
+    NotificationObservable() { 
+        currentNotification = nullptr; 
+    }
+
+    void addObserver(IObserver* obs) override {
+        observers.push_back(obs);
+    }
+
+    void removeObserver(IObserver* obs) override {
+        observers.erase(remove(observers.begin(), observers.end(), obs), observers.end());
+    }
+
+    void notifyObservers() override {
+        for (unsigned int i = 0; i < observers.size(); i++) {
+            observers[i]->update();
+        }
+    }
+
+    void setNotification(INotification* notification) {
+        if (currentNotification != nullptr) {
+            delete currentNotification;
+        }
+        currentNotification = notification;
+        notifyObservers();
+    }
+
+    INotification* getNotification() {
+        return currentNotification;
+    }
+
+    string getNotificationContent() {
+        return currentNotification->getContent();
+    }
+
+    ~NotificationObservable() {
+        if (currentNotification != NULL) {
+            delete currentNotification;
+        }
+    }
+};
+
+class Logger : public IObserver {
+private:
+    NotificationObservable* notificationObservable;
+
+public:
+    Logger(NotificationObservable* observable) {
+        this->notificationObservable = observable;
+    }
+
+    void update() {
+        cout << "Logging New Notification : \n" << notificationObservable->getNotificationContent();
+    }
+};
+
 int main(){
     cout<<"0";
 }
