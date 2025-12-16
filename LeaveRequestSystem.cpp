c19608897953543c39655d34a8d9bad11cac72bb
Shubh Raj <shubhraj625@gmail.com>
2025-12-16 11:28:20 +0530

    edited

--- LeaveRequestSystem.cpp
+++ LeaveRequestSystem.cpp
@@ -1,72 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-class IPosition{
-    protected:
+
+class IPosition {
+protected:
+    IPosition* next = nullptr;
+
+public:
+    virtual ~IPosition() {}
+
+    void setNextHandler(IPosition* n) {
+        next = n;
+    }
+
+    virtual void handleReq(int days) = 0;
+};
+
+class HeadPos : public IPosition {
+public:
+    void handleReq(int days) override {
+        if (days <= 2) {
+            cout << "Approved by the team head\n";
+        } else if (next) {
+            next->handleReq(days);
+        }
+    }
+};
+
+class ManagerPos : public IPosition {
+public:
+    void handleReq(int days) override {
+        if (days <= 7) {
+            cout << "Approved by the team manager\n";
+        } else if (next) {
+            next->handleReq(days);
+        }
+    }
+};
+
+class DirectorPos : public IPosition {
+public:
+    void handleReq(int days) override {
+        if (days <= 10) {
+            cout << "Approved by the team director\n";
+        } else {
+            cout << "Cannot be approved\n";
+        }
+    }
+};
+
+int main() {
     int days;
-    IPosition* next;
-    public:
-    IPosition(int d){
-        next=nullptr;
-        days = d;
-    }
-    void setNextHandler(IPosition* ps){
-        next=ps;
-    }
-    virtual void handleReq() = 0;
-};
-
-class HeadPos : public IPosition{
-    public:
-    HeadPos(int d) : IPosition(d) {} //point
-    void handleReq() override {
-        if(days<=2){
-            cout<<"Approved by the team head\n";
-        }
-        else{
-            next->handleReq();
-        }
-    }
-};
-
-class ManagerPos : public IPosition{
-    public:
-    ManagerPos(int d) : IPosition(d) {} //point
-    void handleReq() override {
-        if(days<=7){
-            cout<<"Approved by the team Manager\n";
-        }
-        else{
-            next->handleReq();
-        }
-    }
-};
-
-class DirectorPos : public IPosition{
-    public:
-    DirectorPos(int d) : IPosition(d) {} //point
-    void handleReq() override {
-        if(days<=10){
-            cout<<"Approved by the team director\n";
-        }
-        else{
-            cout<<"Cannot be approved\n";
-        }
-    }
-};
-
-int main(){
-    int days;
-    cout<<"How many days of leave do you want: ";
+    cout << "How many days of leave do you want: ";
     cin >> days;
-    if(days<=0){
-        cout<<"Are you serious?\n";
+
+    if (days <= 0) {
+        cout << "Are you serious?\n";
         return 0;
     }
-    IPosition* ps = new HeadPos(days);
-    IPosition* ms = new ManagerPos(days);
-    IPosition* ds = new DirectorPos(days);
-    ps->setNextHandler(ms);
-    ms->setNextHandler(ds);
-    ps->handleReq();
+
+    HeadPos head;
+    ManagerPos manager;
+    DirectorPos director;
+
+    head.setNextHandler(&manager);
+    manager.setNextHandler(&director);
+
+    head.handleReq(days);
 }
